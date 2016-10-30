e81ce0a1e39f988b3bd8b55364241e18a6248270
Muhammad Juan Akbar <mail@mjuan.me>
2016-10-30 09:54:25 +0100

    design layout for top recipes and top authors

--- app/modules/home/home.controller.js
+++ app/modules/home/home.controller.js
@@ -53,4 +53,53 @@ function HomeController() {
             "time": "5m"
         }
     ];
+    vm.loadTopRecipes = [
+        {
+            name: "French Onion Soup Gratinee",
+            description: "About as good as it gets! This is the version of French Onion Soup that people seek when they go to restaurants. Healthy and easy.",
+            author: "Nick Slick",
+            "people": "4",
+            "time": "5m"
+        },
+        {
+            name: "Avocado panzanella salad",
+            description: "This high fibre salad is a vibrant mix of tomatoes, avocado and crunchy pieces of ciabatta. Thissalad is full of the authentic flavours of Italy.",
+            author: "Nick Slick",
+            "people": "4",
+            "time": "5m"
+        },
+        {
+            name: "Kale and Feta Salad",
+            description: "Salads can be healthy, satisfying meals on their own or perfect accompaniments to main dishes. We’ve got a great selection of salads.",
+            author: "Nick Slick",
+            "people": "4",
+            "time": "5m"
+        }
+    ];
+    vm.loadTopAuthors = [
+        {
+            name: "Demo 1",
+            photo: ""
+        },
+        {
+            name: "Demo 2",
+            photo: ""
+        },
+        {
+            name: "Demo 3",
+            photo: ""
+        },
+        {
+            name: "Demo 4",
+            photo: ""
+        },
+        {
+            name: "Demo 5",
+            photo: ""
+        },
+        {
+            name: "Demo 6",
+            photo: ""
+        }
+    ];
 }
