1a5d543a359d03ad791bf1bb10f2b9bed1c38a40
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-01 11:18:36 +0100

    setup logger

--- WebApp/app/app.module.js
+++ WebApp/app/app.module.js
@@ -5,4 +5,23 @@ angular
         'app.config',
         'app.shared',
         'app.home'
-    ]);
+    ])
+    .config(['$logProvider', '$provide',
+        function($logProvider, $provide){
+            // Setup Logging/Debug
+            $logProvider.debugEnabled(true);
+
+            $provide.decorator('$log', function($delegate) {
+                var origInfo = $delegate.info, origLog = $delegate.log,
+                    origError = $delegate.error, origWarn = $delegate.warn;
+
+                $delegate.info = function(){if($logProvider.debugEnabled()) origInfo.apply(null, arguments);};
+                $delegate.log = function(){if($logProvider.debugEnabled()) origLog.apply(null, arguments);};
+                $delegate.error = function(){if($logProvider.debugEnabled()) origError.apply(null, arguments);};
+                $delegate.warn = function(){if($logProvider.debugEnabled()) origWarn.apply(null, arguments);};
+
+                return $delegate;
+            });
+        }
+    ])
+;
--- WebApp/app/shared/controllers/shared.controller.js
+++ WebApp/app/shared/controllers/shared.controller.js
@@ -2,17 +2,19 @@ angular
     .module('app.shared')
     .controller('SharedController', SharedController);
 
-SharedController.$inject = ['$state', '$mdSidenav'];
-function SharedController($state, $mdSidenav){
+SharedController.$inject = ['$log', '$state', '$mdSidenav'];
+function SharedController($log, $state, $mdSidenav){
     let vm = this;
     vm.toggleSidenav = toggleSidenav;
     vm.closeSidenav = closeSidenav;
 
     function toggleSidenav(id){
         $mdSidenav(id).toggle();
+        $log.info('ToogleSideNav: %s ', id);
     }
 
     function closeSidenav(id){
         $mdSidenav(id).close();
+        $log.info('CloseSideNav: %s ', id);
     }
 }
