5d1e0412f069b08214cc49444a682c33334aefe1
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-09 18:01:39 +0100

    designing recipe-create

--- WebApp/app/modules/recipe/controllers/recipe.create.controller.js
+++ WebApp/app/modules/recipe/controllers/recipe.create.controller.js
@@ -2,13 +2,14 @@ angular
     .module('app.recipe')
     .controller('RecipeCreateController', RecipeCreateController);
 
-RecipeCreateController.$inject = ['$log'];
-function RecipeCreateController($log) {
+RecipeCreateController.$inject = ['$log', 'Upload'];
+function RecipeCreateController($log, Upload) {
     var vm = this;
     vm.addMoreIngredient = addMoreIngredient;
     vm.removeIngredient = removeIngredient;
     vm.addMoreStep = addMoreStep;
     vm.removeStep = removeStep;
+    vm.uploadImage = uploadImage;
 
     init();
 
@@ -16,6 +17,7 @@ function RecipeCreateController($log) {
         vm.recipe = [];
         vm.ingredients = [{id: 1, name: ""}];
         vm.steps = [{id: 1}];
+        vm.imageName = _generateHash("getcurrentuseremail");
     }
 
     function addMoreIngredient(){
@@ -37,4 +39,37 @@ function RecipeCreateController($log) {
         var lastId = vm.steps.length-1;
         vm.steps.splice(lastId);
     }
+
+    function uploadImage(file) {
+        if(_isMimeTypeAllow(file.name) && !file.$error){
+            file = Upload.rename(file, _changeImgName(file.name));
+
+            Upload.upload({
+                url: 'assets/recipes',
+                data: {file: file}
+            }).then(function(response){$log.info(response)
+            });
+        }
+    }
+
+    function _changeImgName(name){
+        var tmpName = name.toLowerCase().split('.');
+        return vm.imageName + "." + tmpName[tmpName.length-1];
+    }
+
+    function _isMimeTypeAllow(name){
+        var res = name.toLowerCase().split('.');
+        return !!(res[res.length - 1] == 'jpg' || res[res.length - 1] == 'png' || res[res.length - 1] == 'jpeg');
+    }
+
+    function _generateHash(str)
+    {
+        var x = (str.charCodeAt(0) * 719) % 1138;
+        var hash = 837;
+        var i;
+        for (i = 1; i <= str.length; i++)
+            hash = (hash * i + 5 + (str.charCodeAt(i - 1) - 64) * x) % 98503;
+        return hash.toString();
+    }
+
 }
