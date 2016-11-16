af9e717e26de3e8bf17c427c104024c44f9dae17
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-16 01:48:21 +0100

    remove material-icon on webapp

--- WebApp/app/modules/home/home.controller.js
+++ WebApp/app/modules/home/home.controller.js
@@ -7,27 +7,27 @@ function HomeController() {
     vm.loadCategories = [
         {
             "name": "Categorie 1",
-            "icon": "home"
+            "icon": "fa-home"
         },
         {
             "name": "Categorie 2",
-            "icon": "home"
+            "icon": "fa-home"
         },
         {
             "name": "Categorie 3",
-            "icon": "home"
+            "icon": "fa-home"
         },
         {
             "name": "Categorie 4",
-            "icon": "home"
+            "icon": "fa-home"
         },
         {
             "name": "Categorie 5",
-            "icon": "home"
+            "icon": "fa-home"
         },
         {
             "name": "Categorie 6",
-            "icon": "home"
+            "icon": "fa-home"
         }
     ];
     vm.loadNewRecipes = [
