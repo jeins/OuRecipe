7bceac2438537f009bc66bc39d94183aa0b856d4
Muhammad Juan Akbar <mail@mjuan.me>
2017-01-25 00:35:36 +0100

    enable add favorite recipe from recipe profile

--- WebApp/app/modules/recipe/controllers/recipe.profile.controller.js
+++ WebApp/app/modules/recipe/controllers/recipe.profile.controller.js
@@ -2,14 +2,16 @@ angular
     .module('app.recipe')
     .controller('RecipeProfileController', RecipeProfileController);
 
-RecipeProfileController.$inject = ['$log', '$stateParams', '$location', 'ApiRecipe', '$sce'];
-function RecipeProfileController($log, $stateParams, $location, ApiRecipe, $sce) {
+RecipeProfileController.$inject = ['$log', '$stateParams', '$location', 'ApiRecipe', '$sce', 'ApiFavorite', 'Session'];
+function RecipeProfileController($log, $stateParams, $location, ApiRecipe, $sce, ApiFavorite, Session) {
     let vm = this;
     vm.trustSrc = trustSrc;
+    vm.addFavorite = addFavorite;
 
     init();
 
     function init(){
+        vm.currUser = Session.getUser();
         vm.recipeId = $stateParams.recipeId;
 
         if(vm.recipeId === undefined) $location.path('/recipe-search');
@@ -25,7 +27,30 @@ function RecipeProfileController($log, $stateParams, $location, ApiRecipe, $sce)
         ApiRecipe.getRecipeById({recipeId: vm.recipeId}, function(res){
             vm.recipe = res;
 
+            _isUserFavorite();
+
             $log.info("SelectedRecipe: " + JSON.stringify(res));
+        });
+    }
+
+    function addFavorite(){
+        let reqBody = {
+            data: {
+                userId: vm.currUser.id,
+                recipeId: vm.recipe.id
+            }
+        };
+
+        ApiFavorite.addFavoriteRecipe(reqBody, function(res){
+            vm.favoriteStyle = {"color": "red"};
+        });
+    }
+
+    function _isUserFavorite(){
+        vm.recipe.favorites.forEach(function(favorite){console.log(favorite.userId); console.log(vm.currUser.id);
+            if(favorite.userId === vm.currUser.id){
+                vm.favoriteStyle = {"color": "red"};
+            }
         });
     }
 }
